b2b96bf8cb9f242128a241bf2af38747b4098807
Sayakhatova <s.naz03@mail.ru>
2022-03-12 01:15:25 +0600

    .

---
+++ lab4/my-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    products = products;
+
+    share(link: String, text: String) {
+        window.open('https://telegram.me/share/url?url=' + link, '_blank')
+    }
+
+    onNotify() {
+        window.alert('You will be notified when the product goes on sale');
+    }
+}
---
+++ lab4/my-app/src/app/products.ts
@@ -0,0 +1,92 @@
+export const products = [
+    {
+        id: 1,
+        name: 'BENGOO G9000 Stereo Gaming Headset for PS4 PC Xbox One PS5 Controller, Noise Cancelling Over Ear Headphones with Mic, LED Light, Bass Surround, Soft Memory Earmuffs for Laptop Mac Nintendo NES Games',
+        price: 49.99,
+        description: 'Multi-Platform Compatible, Surrounding Stereo Subwoofer, Surrounding Stereo Subwoofer, Surrounding Stereo Subwoofer, Effortlessly Volume Control',
+        rating: 4.3,
+        link: 'https://www.amazon.com/BENGOO-G9000-Controller-Cancelling-Headphones/dp/B01H6GUCCQ/ref=sr_1_4?keywords=gaming+headsets&pd_rd_r=e9f199b4-6caf-4a6d-af06-bc80de6f6fdd&pd_rd_w=VSAq5&pd_rd_wg=vIEaS&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=GEVF6T19NPEV7203875P&qid=1647019852&sr=8-4',
+        image: 'https://m.media-amazon.com/images/I/61CGHv6kmWL._AC_SL1000_.jpg'
+    },
+    {
+        id: 2,
+        name: 'Redragon S101 Wired Gaming Keyboard and Mouse Combo RGB Backlit Gaming Keyboard with Multimedia Keys Wrist Rest and Red Backlit Gaming Mouse 3200 DPI for Windows PC Gamers (Black)',
+        price: 35.98,
+        description: 'PC GAMING KEYBOARD AND GAMING MOUSE COMBO, RGB BACKLIT GAMING KEYBOARD, MULTI MEDIA & ANTI GHOSTING, WIRED GAMING MOUSE, PC GAMING KEYBOARD AND GAMING MOUSE COMPATIBILITY',
+        rating: 4.6,
+        link: 'https://www.amazon.com/Redragon-S101-Keyboard-Ergonomic-Programmable/dp/B00NLZUM36/ref=sr_1_4?keywords=gaming%2Bkeyboard&pd_rd_r=e9f199b4-6caf-4a6d-af06-bc80de6f6fdd&pd_rd_w=VSAq5&pd_rd_wg=vIEaS&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=GEVF6T19NPEV7203875P&qid=1647020336&sr=8-4&th=1',
+        image: 'https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_SL1500_.jpg'
+    },
+    {
+        id: 3,
+        name: 'Flagship Newest Play Station 4 1TB HDD Only on Playstation PS4 Console Slim Bundle with Three Games: The Last of Us, God of War, Horizon Zero Dawn 1TB HDD Dualshock 4 Wireless Controller - Jet Black',
+        price: 620.94,
+        description: 'Brand: Sony, Connectivity Technology:	Wireless, Memory Storage Capacity: 1 TB, Wireless Communication Technology:	Bluetooth',
+        rating: 4.6,
+        link: 'https://www.amazon.com/Flagship-Newest-Station-Playstation-Console/dp/B08289199B/ref=sr_1_7?keywords=playstation+consoles&pf_rd_i=23508887011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=434db2ed-6d53-4c59-b173-e8cd550a2e4f&pf_rd_r=AZZR4XNX4P3H8X73S18B&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1647020491&s=videogames&sr=1-7',
+        image: 'https://m.media-amazon.com/images/I/51RpSkPbtrL._AC_SL1000_.jpg'
+    },
+    {
+        id: 4,
+        name: 'Samsung Galaxy Active Smartwatch 40mm with Extra Charging Cable, Rose Gold - SM-R500NZDCXAR (Renewed)',
+        price: 124.00,
+        description: 'Product works and looks like new. Backed by the 90-day Amazon Renewed Guarantee. - This pre- owned product has been professionally inspected, tested and cleaned by Amazon - qualified suppliers. - There will be no visible cosmetic imperfections when held at an arm length.',
+        rating: 4.5,
+        link: 'https://www.amazon.com/Samsung-Galaxy-Watch-Active-Renewed/dp/B07VT83LXH/ref=sr_1_26?crid=3TX3YLZRD3ICV&keywords=smart%2Bwatch&qid=1647021020&sprefix=smart%2B%2Cspecialty-aps%2C376&sr=8-26&th=1',
+        image: 'https://m.media-amazon.com/images/I/715O06FaG4L._AC_SL1500_.jpg'
+    },
+    {
+        id: 5,
+        name: 'Redragon GS520 Anvil RGB Desktop Speakers, 2.0 Channel PC Computer Stereo Speaker with 6 Colorful LED Modes, Enhanced Sound and Easy-Access Volume Control, USB Powered w/ 3.5mm Cable',
+        price: 49.99,
+        description: 'Enhanced Clear Sound - Equipped with advanced sound drive unit with full range 2.0 channel enhanced stereo core, GS520 ANVIL offers you superior auditory feast. Touch Controlled RGB Lighting - Not only the sound quality is solid, but also the lighting bar is attractive. 6 modes are choosable for different vibes by touch - control, switch off the light is also available.',
+        rating: 4.5,
+        link: 'https://www.amazon.com/Redragon-Speakers-Computer-Colorful-Easy-Access/dp/B08GK9LCRW/ref=pd_rhf_se_s_gcx-rhf_11/135-7977785-6190019?pd_rd_w=Uj2Lw&pf_rd_p=2ae4ccb1-7034-4114-8654-5ba995870d70&pf_rd_r=4TNFS0C1N48YV586GFR8&pd_rd_r=963e575c-9fb9-4ee7-aa07-437ac647f251&pd_rd_wg=Vyp94&pd_rd_i=B08GK9LCRW&psc=1',
+        image: 'https://m.media-amazon.com/images/I/81+PD8UuhhL._AC_SL1500_.jpg'
+    },
+    {
+        id: 6,
+        name: 'Fujifilm Instax Mini 11 Instant Camera Lilac Purple + Fuji Film Value Pack (40 Sheets) + Shutter Accessories Bundle, Incl. Compatible Carrying Case, Selfie Lens, Quicksand Beads Photo Album 64 Pockets',
+        price: 128.95,
+        description:'Includes FujiFilm Mini 11 Camera Lilac Purple, The Fujifilm Mini 11 Camera Lilac Purple features a Fujinon 60mm f/12.7 Lens, Optical Image Viewfinder, Auto Exposure and a Built-In Flash. Instantly produces image prints fun and simple. Fujifilm Mini 11 Camera BUNDLE is Colorful, Fun, Compact and really Cool.',
+        rating: 4.6,
+        link: 'https://www.amazon.com/Fujifilm-Accessories-Compatible-Carrying-Quicksand/dp/B09Q9Y4YS5/ref=sr_1_13_sspa?qid=1647021299&s=electronics&sr=1-13-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzOFRHUFQ5SkdSWVBOJmVuY3J5cHRlZElkPUEwMjM1ODIzOVJTNzZZSjgxQVhRJmVuY3J5cHRlZEFkSWQ9QTAyMzIwOTVSVUVQWklJSkNENFgmd2lkZ2V0TmFtZT1zcF9tdGZfYnJvd3NlJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1',
+        image: 'https://m.media-amazon.com/images/I/614XbxMBSSL._AC_.jpg'
+    },
+    {
+        id: 7,
+        name: 'Sony Alpha a7II Mirrorless Digital Camera with 28-70mm Lens, Photo Software, SanDisk 128GB SD Card, Battery and Dual Charger, Spider Tripod and 6 Feet HDMI Cable Bundle (7 Items)',
+        price: 1598.00,
+        description: 'Sony Alpha a7II Mirrorless Digital Camera with 28-70mm Lens, Photo Software, SanDisk 128GB SD Card, Battery and Dual Charger, Spider Tripod and 6 Feet HDMI Cable Bundle (7 Items)',
+        rating: 4.1,
+        link: 'https://www.amazon.com/Sony-Mirrorless-Digital-28-70mm-Accessory/dp/B07K1X5MV7/ref=sr_1_30_sspa?qid=1647021390&s=electronics&sr=1-30-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFSUEZQWE40MlZFM0UmZW5jcnlwdGVkSWQ9QTA0ODQyMzEzR1E4VzRGVE1BTFg5JmVuY3J5cHRlZEFkSWQ9QTA0Njk4MzkxUURIMUFXQlU3NFAxJndpZGdldE5hbWU9c3BfYnRmX2Jyb3dzZSZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+        image: 'https://m.media-amazon.com/images/I/81qGEBMOziL._AC_SL1500_.jpg'
+    },
+    {
+        id: 8,
+        name: 'Lenovo Flex 5 Laptop, 14.0" FHD Touch Display, AMD Ryzen 5 5500U, 16GB RAM, 256GB Storage, AMD Radeon Graphics, Windows 11 Home',
+        price: 619.00,
+        description: 'Powered by an AMD Ryzen 5 5500U mobile processor with Radeon graphics, this portable notebook computer has 16GB DDR4 RAM and 256GB NVMe SSD Storage, Reduce the clutter - The Flex 5 14 touchscreen laptop has a narrow bezel on 4 sides for more viewing area and less clutter on the IPS FHD(1920 x 1080) display',
+        rating: 4.4,
+        link: 'https://www.amazon.com/Lenovo-Processor-Graphics-82HU00JWUS-Graphite/dp/B09BG96KFJ/ref=sr_1_4?qid=1647021551&s=computers-intl-ship&sr=1-4&th=1',
+        image: 'https://m.media-amazon.com/images/I/71zZiQGzc5L._AC_SL1500_.jpg'
+    },
+    {
+        id: 9,
+        name: 'ALLDOCUBE X Game Tablet PC, Android 11, 10.5" Tablet, Helio P90 CPU, 8GB RAM/128GB ROM, Full fit HD IPS Screen, Type-C, 7500mAh Battery,2.4+5G WiFi,Bluetooth5.0,Support Game, Office, Paint Modes',
+        price: 279.99,
+        description: '【8GB RAM 128GB ROM】10.5-inch narrow bezel design tablet PC that with 1920×1280 full HD resolution and full viewing angle IPS display is equipped with 8GB of memory to meet daily office and entertainment needs. 128GB storage brings you a more extreme experience.',
+        rating: 4.0,
+        link: 'https://www.amazon.com/ALLDOCUBE-Android-7500mAh-Battery-Bluetooth5-0/dp/B09SFX1KYC/ref=sr_1_14_sspa?qid=1647021625&s=computers-intl-ship&sr=1-14-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMElIV1ZFTExMT1JXJmVuY3J5cHRlZElkPUEwNDMxNTQxWkc1NFpKNDJaVE45JmVuY3J5cHRlZEFkSWQ9QTA2MjIxMzQyOEMzOENYQzJIUjEwJndpZGdldE5hbWU9c3BfbXRmX2Jyb3dzZSZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+        image: 'https://m.media-amazon.com/images/I/61nNYx6SdML._AC_SL1500_.jpg'
+    },
+    {
+        id: 10,
+        name: 'CYBERPOWERPC Gamer Xtreme VR Gaming PC, Intel Core i5-11400F 2.6GHz, 8GB DDR4, GeForce RTX 2060 6GB, 500GB NVMe SSD, WiFi Ready & Win 11 Home (GXiVR8060A11)',
+        price: 1013.00,
+        description: 'System: Intel Core i5-11400F 2.6GHz 6-Core | Intel B560 Chipset | 8GB DDR4 | 500GB PCI-E NVMe SSD | Genuine Windows 11 Home 64-bit,Graphics: NVIDIA GeForce RTX 2060 6GB Video Card | 1x HDMI | 1x DisplayPort',
+        rating: 4.5,
+        link: 'https://www.amazon.com/CYBERPOWERPC-Xtreme-i5-11400F-GeForce-GXiVR8060A11/dp/B09DHP9M9G/ref=sr_1_9?qid=1647021625&s=computers-intl-ship&sr=1-9',
+        image: 'https://m.media-amazon.com/images/I/81Wx7hw9vwL._AC_SL1500_.jpg'
+    }
+];
